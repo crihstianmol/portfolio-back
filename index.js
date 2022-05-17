9c92ab4f4f7947fa88f71543576a199e9efa10b4
Crihstian Molina <crihstianmol@gmail.com>
2022-05-17 13:35:15 -0500

    Adding index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import dbConnection from './driver/mongo-connection.js';
+import { tipos } from './graphql/types.js';
+import { resolvers } from './graphql/resolvers.js';
+
+
+
+const server = new ApolloServer({
+    typeDefs: tipos,
+    resolvers: resolvers
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.listen({ port: process.env.PORT || 4000 }, async () => {
+    await dbConnection();
+    await server.start();
+
+    server.applyMiddleware({ app });
+
+    console.log('Server is running');
+});
